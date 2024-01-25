f042ecf3d38555883ddd78ca249a403fe592470b
MANISH SINGH PARIHAR <manishsparihar2020@gmail.com>
2024-01-25 23:18:31 +0530

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name = 'wasteDetection',
+    name = 'fireDetection',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Manish Singh Parihar',
+    author_email= 'manishsparihar2020@gmail.com',
     packages= find_packages(),
     install_requires = []
 
